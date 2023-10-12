dc1a2816e9b17443b1ed5193a92ae6bbb031e170
Carlo Tomasin <carlo.tomasin@icare-world.com>
2023-10-12 11:28:32 +0200

    added hanning window

--- main.py
+++ main.py
@@ -8,39 +8,44 @@ def main():
     plt.style.use('seaborn-v0_8-poster')
 
     sr = 2000
-    ts = 1.0/sr
-    t = np.arange(0, 1, ts)
+    ts = 1/sr
+    t = np.arange(0, 10, ts)
 
     wave = lambda freq, ampl, phase = 0: ampl*np.sin(phase + 2*np.pi*freq*t)
 
-    x = wave(1, 3)
-    x += wave(4, 2, np.pi/4)
-    x += wave(7, 0.5, np.pi/3)
-    x += wave(9, 1, np.pi/2)
-    x += wave(10, 1)
+    x = wave(10, 3)
+    x += wave(40, 2, np.pi/4)
+    x += wave(70, 0.5, np.pi/3)
+    x += wave(90, 1, np.pi/2)
+    x += wave(100, 1)
+
+    x = x * np.hanning( x.size )
 
     plt.figure(figsize=(12, 6))
     plt.subplot(131)
     plt.ylabel('Amplitude')
     plt.plot(t, x, 'r')
 
-    X = np.fft.fft(x)
-    N = len(X)
-    n = np.arange(N)
-    T = N/sr
-    freq = n/T
+    
+
+    X = np.fft.fft(x) # the FFT
+    N = len(X) # the bins number
+    n = np.arange(N) # the bin indexes
+    T = N/sr # the sampling period
+    
+    freq = n/T # the sampling frequencies
 
     plt.subplot(132)
     plt.stem(freq, np.abs(X), 'b', markerfmt=' ', basefmt='-b')
     plt.xlabel('Freq (Hz)')
     plt.ylabel('FFT Amplitude |X(freq)')
-    plt.xlim(0, 20)
+    plt.xlim(0, 200)
 
     plt.subplot(133)
     plt.stem(freq, np.angle(X), 'b', markerfmt=' ', basefmt='-b')
     plt.xlabel('Freq (Hz)')
     plt.ylabel('Phase')
-    plt.xlim(0, 20)
+    plt.xlim(0, 100)
 
 
     plt.tight_layout()
